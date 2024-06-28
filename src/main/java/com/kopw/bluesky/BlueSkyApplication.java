75c0879078f6cd4b7b5deb0c1f77f76eedd75251
dong.nguyen <dong.nguyen@cherry-solutions.com>
2024-06-28 15:41:54 +0700

    int project

---
+++ src/main/java/com/kopw/bluesky/BlueSkyApplication.java
@@ -0,0 +1,13 @@
+package com.kopw.bluesky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSkyApplication.class, args);
+	}
+
+}
